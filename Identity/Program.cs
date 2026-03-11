6bf75478f4f8ae23c72ab4a6ef55961d530c79c3
Chris Golden <crgolden@msn.com>
2026-03-10 19:54:25 -0600

    Adding Elasticsearch

--- Identity/Program.cs
+++ Identity/Program.cs
@@ -1,9 +1,13 @@
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
-using Elastic.Clients.Elasticsearch;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
 using Google.Apis.Auth.AspNetCore3;
 using Identity;
 using Identity.Pages.Account.Manage;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
@@ -12,139 +16,182 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Options;
 using Resend;
-using System.Net.Http.Headers;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-var keyVaultSection = builder.Configuration.GetSection(nameof(Azure.Security.KeyVault));
-var corsPolicySection = builder.Configuration.GetSection(nameof(CorsPolicy));
-var sqlConnectionStringBuilderSection = builder.Configuration.GetSection(nameof(SqlConnectionStringBuilder));
+Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
 
-builder.Services
-    .Configure<SqlConnectionStringBuilder>(sqlConnectionStringBuilderSection)
-    .AddDbContext<ApplicationDbContext>((sp, dbContextOptionsBuilder) =>
+try
+{
+    static (IConfigurationSection, IConfigurationSection, IConfigurationSection) GetSections(ConfigurationManager configuration)
     {
-        var sqlConnectionStringBuilder = sp.GetRequiredService<IOptions<SqlConnectionStringBuilder>>().Value;
-        if (!sqlConnectionStringBuilder.IntegratedSecurity)
-        {
-            var secretClient = sp.GetRequiredService<SecretClient>();
-            var sqlServerUserId = secretClient.GetSecret("SqlServerUserId");
-            var sqlServerPassword = secretClient.GetSecret("SqlServerPassword");
-            sqlConnectionStringBuilder.UserID = sqlServerUserId.Value.Value;
-            sqlConnectionStringBuilder.Password = sqlServerPassword.Value.Value;
-        }
+        var keyVaultSection = configuration.GetSection(nameof(Azure.Security.KeyVault));
+        var corsPolicySection = configuration.GetSection(nameof(CorsPolicy));
+        var sqlConnectionStringBuilderSection = configuration.GetSection(nameof(SqlConnectionStringBuilder));
+        return (keyVaultSection, corsPolicySection, sqlConnectionStringBuilderSection);
+    }
 
-        dbContextOptionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString, sqlServerDbContextOptionsBuilder =>
-        {
-            var assembly = typeof(ApplicationDbContext).Assembly;
-            sqlServerDbContextOptionsBuilder.MigrationsAssembly(assembly);
-        });
-    })
-    .AddIdentity<IdentityUser<Guid>, IdentityRole<Guid>>(identityOptions =>
+    static (Uri, Uri) GetUris(ConfigurationManager configuration)
     {
-        identityOptions.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
-        identityOptions.SignIn.RequireConfirmedAccount = true;
-    })
-    .AddDefaultUI()
-    .AddDefaultTokenProviders()
-    .AddEntityFrameworkStores<ApplicationDbContext>().Services
-    .AddIdentityServer(identityServerOptions =>
+        var elasticsearchNode = configuration.GetValue<Uri>("ElasticsearchNode") ?? throw new InvalidOperationException("Invalid 'ElasticsearchNode'.");
+        var keyVaultUrl = configuration.GetValue<Uri>("KeyVaultUri") ?? throw new InvalidOperationException("Invalid 'KeyVault.VaultUri'.");
+        return (elasticsearchNode, keyVaultUrl);
+    }
+
+    static async Task<(KeyVaultSecret, KeyVaultSecret, KeyVaultSecret, KeyVaultSecret, KeyVaultSecret, KeyVaultSecret, KeyVaultSecret, KeyVaultSecret)> GetSecrets(Uri keyVaultUrl, CancellationToken cancellationToken = default)
     {
-        identityServerOptions.Events.RaiseErrorEvents = true;
-        identityServerOptions.Events.RaiseInformationEvents = true;
-        identityServerOptions.Events.RaiseFailureEvents = true;
-        identityServerOptions.Events.RaiseSuccessEvents = true;
-        identityServerOptions.UserInteraction.ErrorUrl = "/Error";
-    })
-    .AddConfigurationStore<ApplicationDbContext>(configurationStoreOptions => { })
-    .AddOperationalStore<ApplicationDbContext>(operationalStoreOptions => { })
-    .AddAspNetIdentity<IdentityUser<Guid>>()
-    .AddLicenseSummary().Services
-    .AddAuthentication()
-    .AddGoogleOpenIdConnect(
-        authenticationScheme: GoogleOpenIdConnectDefaults.AuthenticationScheme,
-        displayName: nameof(Google),
-        configureOptions: openIdConnectOptions =>
+        var timestamp = Stopwatch.GetTimestamp();
+        var secretClient = new SecretClient(keyVaultUrl, new DefaultAzureCredential());
+        var tasks = new[]
         {
-            openIdConnectOptions.SignInScheme = IdentityConstants.ExternalScheme;
+            secretClient.GetSecretAsync("GravatarApiSecretKey", cancellationToken: cancellationToken),
+            secretClient.GetSecretAsync("ElasticsearchUsername", cancellationToken: cancellationToken),
+            secretClient.GetSecretAsync("ElasticsearchPassword", cancellationToken: cancellationToken),
+            secretClient.GetSecretAsync("SqlServerUserId", cancellationToken: cancellationToken),
+            secretClient.GetSecretAsync("SqlServerPassword", cancellationToken: cancellationToken),
+            secretClient.GetSecretAsync("GoogleClientId", cancellationToken: cancellationToken),
+            secretClient.GetSecretAsync("GoogleClientSecret", cancellationToken: cancellationToken),
+            secretClient.GetSecretAsync("ResendApiToken", cancellationToken: cancellationToken),
+        };
+        var result = await Task.WhenAll(tasks);
+        var elapsedTime = Stopwatch.GetElapsedTime(timestamp);
+        Log.Information("Retrieved secrets from Key Vault in {ElapsedMilliseconds} ms", elapsedTime.Milliseconds);
+        return (result[0].Value, result[1].Value, result[2].Value, result[3].Value, result[4].Value, result[5].Value, result[6].Value, result[7].Value);
+    }
+
+    var builder = WebApplication.CreateBuilder(args);
+    var (keyVaultSection, corsPolicySection, sqlConnectionStringBuilderSection) = GetSections(builder.Configuration);
+    var (elasticsearchNode, keyVaultUrl) = GetUris(builder.Configuration);
+    var (gravatarApiKeySecret, elasticsearchUsername, elasticsearchPassword, sqlServerUserId, sqlServerPassword, googleClientId, googleClientSecret, resendApiToken) = await GetSecrets(keyVaultUrl);
+    builder
+        .ConfigureOpenTelemetry().Services
+        .AddSerilog((sp, loggerConfiguration) => loggerConfiguration
+            .ReadFrom.Configuration(builder.Configuration)
+            .ReadFrom.Services(sp)
+            .Enrich.FromLogContext()
+            .WriteTo.Elasticsearch(
+                [elasticsearchNode],
+                elasticsearchSinkOptions =>
+                {
+                    elasticsearchSinkOptions.BootstrapMethod = BootstrapMethod.Failure;
+                },
+                transportConfiguration =>
+                {
+                    var header = new BasicAuthentication(elasticsearchUsername.Value, elasticsearchPassword.Value);
+                    transportConfiguration.Authentication(header);
+                }))
+        .Configure<SqlConnectionStringBuilder>(sqlConnectionStringBuilderSection)
+        .AddDbContext<ApplicationDbContext>((sp, dbContextOptionsBuilder) =>
+        {
+            var sqlConnectionStringBuilder = sp.GetRequiredService<IOptions<SqlConnectionStringBuilder>>().Value;
+            if (!sqlConnectionStringBuilder.IntegratedSecurity)
+            {
+                sqlConnectionStringBuilder.UserID = sqlServerUserId.Value;
+                sqlConnectionStringBuilder.Password = sqlServerPassword.Value;
+            }
+
+            dbContextOptionsBuilder.UseSqlServer(sqlConnectionStringBuilder.ConnectionString, sqlServerDbContextOptionsBuilder =>
+            {
+                var assembly = typeof(ApplicationDbContext).Assembly;
+                sqlServerDbContextOptionsBuilder.MigrationsAssembly(assembly);
+            });
+        })
+        .AddIdentity<IdentityUser<Guid>, IdentityRole<Guid>>(identityOptions =>
+        {
+            identityOptions.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
+            identityOptions.SignIn.RequireConfirmedAccount = true;
+        })
+        .AddDefaultUI()
+        .AddDefaultTokenProviders()
+        .AddEntityFrameworkStores<ApplicationDbContext>().Services
+        .AddIdentityServer(identityServerOptions =>
+        {
+            identityServerOptions.Events.RaiseErrorEvents = true;
+            identityServerOptions.Events.RaiseInformationEvents = true;
+            identityServerOptions.Events.RaiseFailureEvents = true;
+            identityServerOptions.Events.RaiseSuccessEvents = true;
+            identityServerOptions.UserInteraction.ErrorUrl = "/Error";
+        })
+        .AddConfigurationStore<ApplicationDbContext>(configurationStoreOptions => { })
+        .AddOperationalStore<ApplicationDbContext>(operationalStoreOptions => { })
+        .AddAspNetIdentity<IdentityUser<Guid>>()
+        .AddLicenseSummary().Services
+        .AddAuthentication()
+        .AddGoogleOpenIdConnect(
+            authenticationScheme: GoogleOpenIdConnectDefaults.AuthenticationScheme,
+            displayName: nameof(Google),
+            configureOptions: openIdConnectOptions =>
+            {
+                openIdConnectOptions.SignInScheme = IdentityConstants.ExternalScheme;
+                openIdConnectOptions.ClientId = googleClientId.Value;
+                openIdConnectOptions.ClientSecret = googleClientSecret.Value;
+            }).Services
+        .Configure<ResendClientOptions>(configureOptions =>
+        {
+            configureOptions.ApiToken = resendApiToken.Value;
+        })
+        .AddHttpClient<ResendClient>().Services
+        .AddTransient<IResend, ResendClient>()
+        .AddTransient<IEmailSender, EmailSender>()
+        .AddHttpClient<IGravatar, Gravatar>((sp, httpClient) =>
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(IdentityConstants.BearerScheme, gravatarApiKeySecret.Value);
         }).Services
-    .AddTransient<IPostConfigureOptions<OpenIdConnectOptions>, OpenIdConnectConfigureOptions>()
-    .AddOptions<ResendClientOptions>().Services
-    .AddTransient<IPostConfigureOptions<ResendClientOptions>, ResendClientConfigureOptions>()
-    .AddHttpClient<ResendClient>().Services
-    .AddTransient<IResend, ResendClient>()
-    .AddTransient<IEmailSender, EmailSender>()
-    .AddHttpClient<IGravatar, Gravatar>((sp, httpClient) =>
+        .AddScoped<IAvatarService, GravatarService>()
+        .AddRazorPages().Services
+        .Configure<CorsPolicy>(corsPolicySection)
+        .AddCors()
+        .AddHealthChecks().AddDbContextCheck<ApplicationDbContext>().Services
+        .AddAzureClients(configureClients =>
+        {
+            configureClients.AddSecretClient(keyVaultSection);
+        });
+    if (builder.Environment.IsDevelopment())
     {
-        var secretClient = sp.GetRequiredService<SecretClient>();
-        var gravatarApiKeySecret = secretClient.GetSecret("GravatarApiSecretKey").Value;
-        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(IdentityConstants.BearerScheme, gravatarApiKeySecret.Value);
-    }).Services
-    .AddScoped<IAvatarService, GravatarService>()
-    .AddRazorPages().Services
-    .Configure<CorsPolicy>(corsPolicySection)
-    .AddCors()
-    .AddHealthChecks().AddDbContextCheck<ApplicationDbContext>().Services
-    .AddAzureClients(configureClients =>
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        builder.Services.Configure<IdentityPasskeyOptions>(options =>
+        {
+            // Allow https://localhost:7261 origin.
+            options.ValidateOrigin = context => ValueTask.FromResult(context.Origin == "https://localhost:7261");
+        });
+    }
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    app.UseSerilogRequestLogging();
+    app.UseExceptionHandler("/Error");
+    if (app.Environment.IsDevelopment())
     {
-        configureClients.AddSecretClient(keyVaultSection);
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseRouting();
+    app.UseIdentityServer();
+    app.UseCors(corsPolicyBuilder =>
+    {
+        var corsPolicy = app.Services.GetRequiredService<IOptions<CorsPolicy>>().Value;
+        corsPolicyBuilder.WithOrigins(corsPolicy.Origins.ToArray());
     });
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-    builder.Services.Configure<IdentityPasskeyOptions>(options =>
-    {
-        // Allow https://localhost:7261 origin.
-        options.ValidateOrigin = context => ValueTask.FromResult(context.Origin == "https://localhost:7261");
-    });
+    app.UseAuthorization();
+    app.MapAdditionalIdentityEndpoints();
+    app.MapStaticAssets();
+    app.MapHealthChecks("health");
+    app.MapRazorPages()
+       .WithStaticAssets()
+       .RequireAuthorization();
+    await app.RunAsync();
 }
-
-var app = builder.Build();
-
-var uri = new Uri("http://192.168.0.33:9200/");
-var settings = new ElasticsearchClientSettings(uri);
-var client = new ElasticsearchClient(settings);
-await client.Indices.CreateAsync("identity");
-var doc1 = new
+catch (Exception ex)
 {
-    Id = 1,
-    User = "flobernd",
-    Message = "Trying out the client, so far so good?"
-};
-
-var response1 = await client.IndexAsync(doc1, "identity");
-var response2 = await client.GetAsync<object>(1, idx => idx.Index("identity"));
-if (response2.IsValidResponse)
-{
-    var doc2 = response2.Source;
+    Log.Fatal(ex, "Application terminated unexpectedly");
 }
-
-var response3 = await client.DeleteAsync("identity", 1);
-
-// Configure the HTTP request pipeline.
-app.UseExceptionHandler("/Error");
-if (app.Environment.IsDevelopment())
+finally
 {
-    app.UseMigrationsEndPoint();
+    await Log.CloseAndFlushAsync();
 }
-else
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseIdentityServer();
-app.UseCors(corsPolicyBuilder =>
-{
-    var corsPolicy = app.Services.GetRequiredService<IOptions<CorsPolicy>>().Value;
-    corsPolicyBuilder.WithOrigins(corsPolicy.Origins.ToArray());
-});
-app.UseAuthorization();
-app.MapAdditionalIdentityEndpoints();
-app.MapStaticAssets();
-app.MapHealthChecks("health");
-app.MapRazorPages()
-   .WithStaticAssets()
-   .RequireAuthorization();
-app.Run();
